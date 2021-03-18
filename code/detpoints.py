38d5cd04bdcf961be152842d5b6514e99c9ceb0a
Carlos de Gois <carlosbgois@gmail.com>
2021-03-17 22:55:52 -0300

    Generalized symmetries listing

--- code/detpoints.py
+++ code/detpoints.py
@@ -1,4 +1,6 @@
 """
+Prepare-and-measure scenario deterministic strategies generation.
+
 Usage: call "python detpoints.py --help"
 """
 
@@ -7,6 +9,7 @@ from itertools import product, chain, permutations
 from random import sample, randrange, randint
 
 import numpy as np
+from more_itertools import random_product
 
 
 def iselement(els, l):
@@ -56,12 +59,12 @@ def independent_strategies(ma, mb, my, samples=0, segmented=False):
     detp = [[int(digit) for digit in el] for el in detp]
 
     if segmented:
-        return [[detp[i][j:j+my] for j in range(0, width, my)] for i in range(n_lambdas)]
+        return [[detp[i][j:j+my] for j in range(0, width, my)] for i in range(len(detp))]
     else:
         return detp
 
 
-def detpoints(ma, mb, mx, my, samples=0):
+def detpoints(ma, mb, mx, my, samples=0, binary=False):
     """Build a PAM scenario's deterministic strategies.
 
     Sample independent strategies for `ma` preparations then repeat `mx - ma` blocks
@@ -73,25 +76,35 @@ def detpoints(ma, mb, mx, my, samples=0):
         mx: nof. total preparations.
         my: nof. available measurements.
         samples: if `0`, generates all strategies, else sample `samples` strategies.
+        binary: if `True`, will expand the `mb`-ary basis in `mb` coefficients with
+            a single `1` indicating the obtained outcome. E.g.: will turn a strategy
+            [0, 2, 1, 0] into [1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0], which is the more
+            conventional way of listing strategies.
 
     Returns:
         list: each row is a strategy in which the coefficients `i` stand for the
             deterministic result represented in the given strategy. Example: with
-            `ma = 2`, `mb = 3`, `mx = 3`, `my = 2`, a row `[[0, 2], [1, 0], [0, 2]]`
+            `ma = 2`, `mb = 3`, `mx = 3`, `my = 2`, a row `[0, 2, 1, 0, 0, 2]`
             is the strategy where measurements y in preparations x return the i-th
             result. A conventional ordering is to take the lexicographic one:
-            `[[(x0, y0), (x0, y1)], [(x1, y0), (x1, y1)], [(x2, y0), (x2, y1)]]`
+            `[(x0, y0), (x0, y1), (x1, y0), (x1, y1), (x2, y0), (x2, y1)]`
 
     Todo:
-        * Sampling version (use more_itertools.random_product)
+        * binary version
     """
 
     indeps = np.asarray(independent_strategies(ma, mb, my, samples, segmented=True))
 
-    # Indexes to include needed redundant segments in all allowed positions.
-    orderings = [r for r in product(range(ma), repeat=mx) if iselement(range(ma), r)]
-    detps = indeps[:,orderings,:].reshape(-1, mx * my)
+    if samples:
+        # Find one allowed random ordering for the segments.
+        orderings = random_product(range(ma), repeat=mx)
+        while not iselement(range(ma), orderings):
+            orderings = random_product(range(ma), repeat=mx)
+    else:
+        # Find *all* allowed oderings for the segments (thus generating *all* det. points).
+        orderings = [r for r in product(range(ma), repeat=mx) if iselement(range(ma), r)]
 
+    detps = indeps[:,orderings,:].reshape(-1, mx * my)
     return np.unique(detps, axis=0)
 
 
@@ -99,40 +112,45 @@ def symmetries(mb, mx, my):
     """List all symmetries in the behaviors.
 
     Todo:
-        * Clearer procedure for measurement permutation symmetries.
-        * Include combined symmetries (measurement + preparation).
-        * Extend for mb > 2.
+        * Testing
     """
 
-    names = [[f"p(0|{x}{y})" for y in range(1, my + 1)] for x in range(1, mx + 1)]
+    names = [[[f"p({b}|{x}{y})" for b in range(mb)] for y in range(my)] for x in range(mx)]
 
-    # A very bad way of doing it:
-    meas_perms = []
-    for perm in permutations(range(my), my):
-        equiv = [np.asarray(names)[prep][[perm]] for prep in range(mx)]
-        meas_perms.append(np.asarray(equiv).reshape(1, -1))
-    meas_perms = np.asarray(meas_perms).reshape(np.math.factorial(my), -1)
-    meas_perms = "\n".join([" ".join(p) for p in meas_perms])
+    outcome_perms = list(permutations(range(mb)))
+    prep_perms = list(permutations(range(mx)))
+    meas_perms = list(permutations(range(my)))
 
-    prep_perms = list(permutations(names, mx))
-    prep_perms = np.asarray(prep_perms).reshape(np.math.factorial(mx), mx * my)
-    prep_perms = "\n".join([" ".join(p) for p in prep_perms])
-
-    names = " ".join(np.asarray(names).reshape(mx * my))
-
-    return "Names\n" + names + "\nMaps\n" + meas_perms + "\n" + prep_perms + "\n"
+    maps = np.asarray(names)[:,:,outcome_perms]
+    maps = np.asarray([maps[prep_perms,:,:,i] for i in range(mb)])
+    maps = np.asarray([maps[i,j,:,meas_perms,:]
+                       for i in range(maps.shape[0])
+                       for j in range(maps.shape[1])])
 
 
-def export(detps, fname, named, lrs, mx, my):
-    """Export detps to an lrs or PANDA-readable file, overwriting fname if it exists.
+    maps = "\n".join([" ".join(m) for m in maps.reshape(-1, mb * mx * my)])
+    names = " ".join(np.asarray(names).reshape(mb * mx * my))
 
-    Each row is a vertex p(b = 0 | a, y).
+    return names, maps
 
-    First line is the file header, and the second adds a "1" before each vertex
-    to indicate it's a vertex and not a ray, as specified in lrs's user guide.
 
-    TODO:
-    - Write the 'maps' section in PANDA output to filter redundant inequalities.
+def export(mb, mx, my, detps, fname, named=True, symmetries=True, lrs=False):
+    """Export `detps` to an lrs or PANDA-readable file named `fname` (will overwrite).
+
+    Vertices of `detps` should be given row-wise. If `lrs`, the output will have
+    `1` before each vertex to indicate it's a vertex and not a ray, as specified
+    in lrs's user guide.
+
+    Args:
+        mb: nof. effects per measurement.
+        mx: nof. total preparations.
+        my: nof. available measurements.
+        detps: row-wise listing of deterministic strategies to format.
+        fname: output file path (will be overwriten).
+        named: if `True`, will add PANDA's "Names" section to ease of reading.
+        symmetries: if `True`, will add PANDA's "Maps" so that its convex hull
+            algorithm will list a single representant for each inequality class.
+        lrs: if `True`, will instead output for lrs (no names and symmetries support).
     """
 
     if lrs:
@@ -140,7 +158,8 @@ def export(detps, fname, named, lrs, mx, my):
         exp += "\n".join([" ".join(map(str, [1, *strategy])) for strategy in detps])
     else:
         if named:
-            exp = symmetries(mb, mx, my)
+            names, maps = symmetries(mb, mx, my)
+            exp = "Names\n" + names + "\nMaps\n" + maps + "\n"
         else:
             exp = ""
         exp += f"Vertices\n" + "\n".join([" ".join(map(str, strategy)) for strategy in detps])
@@ -151,14 +170,15 @@ def export(detps, fname, named, lrs, mx, my):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('ma', type=int, help='Dimension of classical message.')
-    parser.add_argument('mb', type=int, help='Nof. measurement effects.')
-    parser.add_argument('mx', type=int, help='Nof. preparations.')
-    parser.add_argument('my', type=int, help='Nof. measurements')
-    parser.add_argument('output', type=str, help='Output file name')
-    parser.add_argument('--named', action='store_true', help='Named variables')
-    parser.add_argument('--lrs', action='store_true', help='Output in lrs format')
+    parser.add_argument("ma", type=int, help="Dimension of classical message.")
+    parser.add_argument("mb", type=int, help="Nof. measurement effects.")
+    parser.add_argument("mx", type=int, help="Nof. preparations.")
+    parser.add_argument("my", type=int, help="Nof. measurements")
+    parser.add_argument("output", type=str, help="Output file name")
+    parser.add_argument("--nonames", action="store_false", help="No variables names")
+    parser.add_argument("--nosymms", action="store_false", help="Don't list symmetries")
+    parser.add_argument("--lrs", action="store_true", help="Output in lrs format")
     args = parser.parse_args()
 
-    export(detstrategies(args.ma, 2, args.mx, args.my),
-           args.output, args.named, args.lrs, args.mb, args.mx, args.my)
+    detps = detstrategies(args.ma, args.mb, args.mx, args.my)
+    export(args.mb, args.mx, args.my, detps, args.fname, args.nonames, args.nosymms, args.lrs)
