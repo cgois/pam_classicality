92088de1fda8224cbe333a4c83fff88197bc049b
Carlos de Gois <carlosbgois@gmail.com>
2021-03-19 02:54:00 -0300

    Reimplemented method for preparation classicality.

--- code/classicalpreparations.py
+++ code/classicalpreparations.py
@@ -1,109 +1,107 @@
+from itertools import chain
 from random import sample, randrange, randint
 from typing import Union, List, Dict
+
 import numpy as np
-import picos
+import picos as pic
 
-from measurements import insphere_radius
-from scenario import LocalModel
-
-
-
-class LPM(LocalModel):
-
-    def __init__(self, measurements: np.ndarray,
-                 ndetp: int,
-                 optrounds: int = 1,
-                 mx: int = 3,
-                 ma: int = 2,
-                 meastag: str = "untagged_measurement"):
-
-        """Streamline of LPM instantiation with some default parameters."""
-
-        # WARNING: mb != 2 is not implemented! (only proj. measurements on qbits)
-        scenario = {"mx": mx, "ma": ma, "my": measurements.shape[0] // 2, "mb": 2}
-        super().__init__(measurements, scenario, ndetp, optrounds, meastag)
+from detpoints import build_detpoints
+from utils import insphere_radius, matrix2bloch
 
 
-    def det_points(self, ndetp: int, scenario: Dict[str, int]) -> np.ndarray:
-        """Build a table with ndetp randomly sampled extremal points from the LPM polytope.
-        We suppose Alice is the preparation and Bob the (dichotomic) measurements.
-
-        Procedure: to generate one det. point, generate an ma*my length list of random bits,
-        then repeat mx-ma blocks of it at random positions.
-        """
-
-        def independent_strategies(ndetp: int,
-                                   scenario: Dict[str, int]) -> np.ndarray:
-            """Generate a ma*my length list of random bits. (Corresponds to sampling
-            ndetp deterministic strategies for a given nof. meas. and results.)
-
-            A list of ndetp unique integers in the given range is created.
-            (Except if n_lambdas becomes larger than long, in which case uniqueness is dropped).
-
-            Each sampled decimal is cast to a binary string with a suitable fixed length.
-            We then split digits of each string in the list and cast them as an integer.
-            """
-
-            n_lambdas = scenario['mb'] ** (scenario['my'] * scenario['ma'])
-
-            try:
-                random_ints = sample(range(0, n_lambdas), ndetp)
-            except OverflowError: # Can't use unique sampling if n_lambdas is too large.
-                random_ints = [randint(0, n_lambdas) for _ in range(ndetp)]
-
-            detp = ['{:0{}b}'.format(el, scenario['ma'] * scenario['my'])
-                        for el in random_ints]
-
-            return np.array([[int(digit) for digit in el] for el in detp]).T
-
-        # --------------------------
-
-        # Sample ndetp independent strategies and repeat mx-ma blocks:
-        ind_strs = independent_strategies(ndetp, scenario)
-        detps = ind_strs
-        my = scenario['my']
-
-        # NOTE: I'm not picking a different "a" for each strategy, but rather repeating
-        # the whole block at once (effectively: picking the same "a" and repeating at same pos.
-        # for all strategies of the current round. I did it independently before and found no
-        # difference in the results when using many redundant rounds.
-        for _ in range(scenario['mx'] - scenario['ma']): # Nof. repeated blocks.
-            ins_position = randrange(1 + detps.shape[0] // my) * my # Position for repeated block.
-            rep_str = randrange(scenario['ma']) # Which "a" strategy to repeat.
-            detps = np.insert(detps, ins_position,
-                              ind_strs[rep_str * my:(rep_str + 1) * my, :], axis=0)
-
-        return detps
+RTOL = 1E-7
+ATOL = 1E-7
+ZERO_TOL = 1E-6
+SEPARATOR = "-" * 40
 
 
-    def find_model(self, state: np.ndarray,
-                   detpoints: np.ndarray,
-                   verb: int = -1,
-                   solver: str = "gurobi") -> picos.modeling.solution.Solution:
-        """Maximum visibility such that preparation 'state' has an LPM model for given strategies."""
+def max_visibility_classical(preps, meas, detpoints, verb=-1, solver="gurobi"):
+    """Find best PAM-classical model for `preps` given `meas` and `detpoints`.
 
-        radius = insphere_radius(self.measurements)
-        meas, _ = np.split(self.measurements, 2)    # Ignore antipodals.
-        assert np.all(meas == -_), "Measurements were not correctly ordered."
+    Args:
+        preps: list of ndarrays of density operators (all with same dimensions).
+        meas: ordered list of effects (ndarrays) following, e.g. `[Z0, Z1, X0, X1]`.
+        detpoints: row-wise deterministic strategies to consider.
+        verb: solver verbosity level.
+        solver: any LP solver accepted by `picos`.
 
-        weights = picos.RealVariable("weights", detpoints.shape[1])
-        d = picos.RealVariable("visibility", 1)
-        dot = np.inner(state, meas).flatten()
-        behaviors = 0.5 * (1 + d * dot / radius)
+    Returns:
+        picos.Solution: its 'value' attribute is the maximum visibility s.t. a
+            PAM-classical model exists, and in "primals" are the optimized weights
+            for each strategy in `detpoints`.
+    """
 
-        prob = picos.Problem()
-        prob.add_list_of_constraints([d >= 0, d <= 1, weights >= 0, picos.sum(weights) == 1])
-        prob.add_constraint(behaviors == detpoints * weights)    # * is matmul by type inference.
-        prob.set_objective("max", d)
+    dim = preps[0].shape[0]
+    eta = insphere_radius([matrix2bloch(m) for m in meas])
 
-        prob.options.solver = solver
-        prob.options.verbosity = verb
-        prob.license_warnings = False
+    vis = pic.RealVariable("visibility", 1)
+    weights = pic.RealVariable("weights", detpoints.shape[0])
 
-        return prob.solve()
+    prob = pic.Problem()
+    prob.set_objective("max", vis)
+    prob.add_list_of_constraints([vis >= 0, vis <= 1, weights >= 0, pic.sum(weights) == 1])
+
+    behaviors = []
+    for i in range(len(preps)):
+        o_x = (vis / eta) * preps[i] + (1 - vis / eta) * np.eye(dim) / dim
+        for j in range(len(meas)):
+            # [tr(O0 M(0|0)), tr(O1 M(0|0)), ..., tr(O0 M(1|0)), ..., tr(Omx M(mb|my)]
+            behaviors.append(pic.trace(o_x * meas[j]))
+
+    model = detpoints.T * weights
+    prob.add_list_of_constraints([behaviors[i] == model[i] for i in range(len(behaviors))])
+
+    prob.options.solver = solver
+    prob.options.verbosity = verb
+    prob.license_warnings = False
+
+    return prob.solve()
 
 
+def optimize_classical_model(preps, ma, meas, mb, ndetps, rounds,
+                             verb=-1, solver="gurobi"):
+    """Iterative search for the best classical model iteratively.
 
-# TODO: Reimplementar chamada pelo terminal
+    1. Find best model with self.ndetp starting strategies.
+    2. Select the strategies with nonzero weights and discard the others.
+    3. Complete the ones we kept with new randomly sampled strategies and run again.
+    4. Break if optval = 1 or if the opt. values of last 'rounds' rounds were equal.
+
+    Todo:
+        * If take `meas` as, e.g., [[X0, X1], [Z0, Z1]] may infer mb.
+    """
+
+    mx, my = len(preps), len(meas) // mb
+    detpoints = build_detpoints(ma, mb, mx, my, samples=ndetps)
+    optvals = sample(range(-rounds, 0), rounds) # Store `rounds` previous vals.
+    round_count = 0
+
+    if verb >= 0:
+        print(SEPARATOR + "\nOptimizing classical model:\n")
+
+    # Break when optval is 1 or when all last rounds are sufficiently close.
+    while(optvals[-1] != 1 and not
+          np.all(np.isclose(optvals, optvals[-1], rtol=RTOL, atol=ATOL))):
+
+        result = max_visibility_classical(preps, meas, detpoints, verb, solver)
+        optvals.append(result.value)
+        optvals.pop(0)
+
+        # Discard zero-weighted strategies and sample new ones.
+        weights = np.array(result.primals[list(result.primals.keys())[1]])
+        nonzeros = [i for i in range(len(weights)) if weights[i] >= ZERO_TOL]
+        detpoints = np.vstack((detpoints[nonzeros,:],
+                               build_detpoints(ma, mb, mx, my, ndetps - len(nonzeros))))
+
+        if verb >= 0:
+            print(f'round {round_count} -> optval: ' + str(result.value))
+            round_count += 1
+
+    assert detpoints[:,range(len(nonzeros))].shape[1] == weights[nonzeros].shape[0],\
+    'nof. weights != nof. detpoints after optimizing local model!'
+
+    return result
+
+
 if __name__ == '__main__':
-    pass
+    raise NotImplementedError
